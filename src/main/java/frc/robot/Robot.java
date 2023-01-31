6684277e9af76f8433d1e95878f07ef7c9f3ef5c
Haadi Khan <haadikhan05@gmail.com>
2023-01-30 20:10:43 -0500

    Pt. 2 of prior commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,13 +4,6 @@
 
 package frc.robot;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryUtil;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,7 +20,7 @@ public class Robot extends TimedRobot {
 
 	private RobotContainer m_robotContainer;
 	// String trajectoryJSON = "paths/Really noticeable pattern.wpilib.json";
-	
+
 	// Trajectory trajectory = new Trajectory();
 	/**
 	 * This function is run when the robot is first started up and should be used
@@ -39,11 +32,13 @@ public class Robot extends TimedRobot {
 		// and put our
 		// autonomous chooser on the dashboard.
 		// try {
-		// 	Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
-		// 	trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
-		//  } catch (IOException ex) {
-		// 	DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
-		//  }
+		// Path trajectoryPath =
+		// Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+		// trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+		// } catch (IOException ex) {
+		// DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON,
+		// ex.getStackTrace());
+		// }
 		// m_robotContainer = new RobotContainer();
 	}
 
